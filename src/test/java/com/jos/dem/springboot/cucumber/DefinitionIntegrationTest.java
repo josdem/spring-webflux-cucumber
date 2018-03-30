65e016ff0a692e4aecc7a5a57c13a75d4ac29993
josdem <joseluis.delacruz@gmail.com>
2018-03-30 16:37:14 -0600

    Validating list collection values

--- src/test/java/com/jos/dem/springboot/cucumber/DefinitionIntegrationTest.java
+++ src/test/java/com/jos/dem/springboot/cucumber/DefinitionIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.jos.dem.springboot.cucumber;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 import com.jos.dem.springboot.cucumber.model.Person;
@@ -22,6 +23,11 @@ public class DefinitionIntegrationTest extends SpringIntegrationTest {
   @Then("^the client receives persons$")
   public void shouldGetPersosn() throws Exception {      
     assertEquals(5 , persons.size());
+    assertTrue(persons.contains(new Person("josdem", "joseluis.delacruz@gmail.com")));
+    assertTrue(persons.contains(new Person("tgrip", "tgrip@email.com")));
+    assertTrue(persons.contains(new Person("edzero", "edzero@email.com")));
+    assertTrue(persons.contains(new Person("skuarch", "skuarch@email.com")));
+    assertTrue(persons.contains(new Person("jeduan", "jeduan@email.com")));
   }
 
 }
