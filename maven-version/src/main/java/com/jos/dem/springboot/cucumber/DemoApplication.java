3034d92a61b2137fa19ff8e08c4c9d0da3a6811e
josdem <joseluis.delacruz@gmail.com>
2018-07-23 12:59:44 -0400

    Adding webflux bean component

--- maven-version/src/main/java/com/jos/dem/springboot/cucumber/DemoApplication.java
+++ maven-version/src/main/java/com/jos/dem/springboot/cucumber/DemoApplication.java
@@ -2,6 +2,8 @@ package com.jos.dem.springboot.cucumber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +11,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+  @Bean
+  WebClient getWebClient() {
+    return WebClient.create("http://localhost:8080/persons");
+  }
 
 }
