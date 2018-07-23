bb50d45ac61f5c7c7b00cdb0336e2457f31478cc
josdem <joseluis.delacruz@gmail.com>
2018-07-23 10:49:30 -0400

    Ignoring swp and swo files

--- src/main/java/com/jos/dem/springboot/cucumber/DemoApplication.java
+++ src/main/java/com/jos/dem/springboot/cucumber/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-  }  
+  }
 
   @Bean
   WebClient getWebClient() {
