bba28de8b963abdb2af569c568a6d439c062546d
josdem <joseluis.delacruz@gmail.com>
2018-07-23 15:54:09 -0400

    Integrating junit 5 framework in gradle directory

--- gradle-version/src/test/java/com/jos/dem/springboot/cucumber/DefinitionIntegrationTest.java
+++ gradle-version/src/test/java/com/jos/dem/springboot/cucumber/DefinitionIntegrationTest.java
@@ -1,33 +1,30 @@
 package com.jos.dem.springboot.cucumber;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.jos.dem.springboot.cucumber.model.Person;
 
 import java.util.List;
 
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import reactor.core.publisher.Flux;
 
 public class DefinitionIntegrationTest extends SpringIntegrationTest {
-  private List<Person> persons;
-
-  @When("^the client wants persons$")	
-  public void shouldCallPersons() throws Exception {
-    Flux<Person> result = executeGet();  
-    persons = result.collectList().block();
-  }
 
   @Then("^the client receives persons$")
-  public void shouldGetPersons() throws Exception {      
+  public void shouldGetPersons() throws Exception {
+    List<Person> persons = executeGet().collectList().block();
+
     assertEquals(5 , persons.size());
-    assertTrue(persons.contains(new Person("josdem", "joseluis.delacruz@gmail.com")));
-    assertTrue(persons.contains(new Person("tgrip", "tgrip@email.com")));
-    assertTrue(persons.contains(new Person("edzero", "edzero@email.com")));
-    assertTrue(persons.contains(new Person("skuarch", "skuarch@email.com")));
-    assertTrue(persons.contains(new Person("jeduan", "jeduan@email.com")));
+    assertAll("person",
+      () -> assertTrue(persons.contains(new Person("josdem", "joseluis.delacruz@gmail.com"))),
+      () -> assertTrue(persons.contains(new Person("tgrip", "tgrip@email.com"))),
+      () -> assertTrue(persons.contains(new Person("edzero", "edzero@email.com"))),
+      () -> assertTrue(persons.contains(new Person("skuarch", "skuarch@email.com"))),
+      () -> assertTrue(persons.contains(new Person("jeduan", "jeduan@email.com")))
+    );
   }
 
 }
