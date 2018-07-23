3025a76d32656bac02a5ab899d977f7f8127be1d
josdem <joseluis.delacruz@gmail.com>
2018-07-23 11:29:04 -0400

    Creating Gradle project structure

--- src/test/java/com/jos/dem/springboot/cucumber/CucumberTest.java
+++
@@ -1,9 +0,0 @@
-package com.jos.dem.springboot.cucumber;
-
-import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources")
-public class CucumberTest {}
--- src/test/resources/person.feature
+++
@@ -1,4 +0,0 @@
-Feature: Persons can be retrieved  
-  Scenario: client makes call to GET persons    
-    When the client wants persons
-    Then the client receives persons
