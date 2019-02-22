8c91bf76eb5411b6cf4ff8caea4e5a183997f85f
josdem <joseluis.delacruz@gmail.com>
2019-02-22 07:50:49 -0500

    #3 Adding license Apache in src

--- src/main/java/com/jos/dem/springboot/cucumber/service/impl/PersonServiceImpl.java
+++ src/main/java/com/jos/dem/springboot/cucumber/service/impl/PersonServiceImpl.java
@@ -1,3 +1,16 @@
+/*
+  Copyright 2019 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package com.jos.dem.springboot.cucumber.service.impl;
 
 import javax.annotation.PostConstruct;
@@ -22,16 +35,16 @@ public class PersonServiceImpl implements PersonService {
 
   @PostConstruct
   public void setup(){
-    Stream.of(new Person("josdem", "joseluis.delacruz@gmail.com"), 
-              new Person("tgrip", "tgrip@email.com"), 
+    Stream.of(new Person("josdem", "joseluis.delacruz@gmail.com"),
+              new Person("tgrip", "tgrip@email.com"),
               new Person("edzero", "edzero@email.com"),
               new Person("skuarch", "skuarch@email.com"),
               new Person("jeduan", "jeduan@email.com"))
-      .forEach(person -> persons.put(person.getNickname(), person));   
+      .forEach(person -> persons.put(person.getNickname(), person));
   }
 
   public Flux<Person> getAll(){
-    return Flux.fromIterable(persons.values());    
+    return Flux.fromIterable(persons.values());
   }
 
   public Mono<Person> getByNickname(String nickname){
