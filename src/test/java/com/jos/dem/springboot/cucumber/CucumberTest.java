9546f3f6c95cf0723d84440d240453efc4031097
josdem <joseluis.delacruz@gmail.com>
2019-04-10 16:31:05 -0400

    #6 Adding Json report config

--- src/test/java/com/jos/dem/springboot/cucumber/CucumberTest.java
+++ src/test/java/com/jos/dem/springboot/cucumber/CucumberTest.java
@@ -25,7 +25,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources",
-                 format = "pretty",
+                 format = {"pretty","json:target/reports/cucumber.json"},
                  plugin = "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/reports/report.html")
 public class CucumberTest {
 
